4473a9b862b7dd960b29bb1d897ad534dd74089f
Yong <legeous113101@gmail.com>
2021-11-19 13:06:25 +0800

    delete state machine

---
+++ Learning/Learning/FooClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Learning
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    class Node
+    {
+        public int manhattanDistance;
+        public int cost;
+    }
+
+    class AstarTest
+    {
+        List
+    }
+}
---
+++ Learning/Learning/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class AstarTest
+    {
+        Node[,] nodes;
+        static AstarTest test;
+        public static AstarTest Instance
+        {
+            get
+            {
+                if (test == null) test = new AstarTest {width = 10, height = 10};
+                return test;
+            }
+        }
+        public int width, height;
+    }
+
+    class Node
+    {
+        public int ManhattanDistance
+        {
+            get
+            {
+                if (x < 0 || y < 0) throw new Exception();
+                int rtx = AstarTest.Instance.width - (x + 1);
+                int rty = AstarTest.Instance.width - (y + 1);
+                return rtx + rtx;
+            }
+        }
+        public int cost;
+        public int x = -1;
+        public int y = -1;
+    }
+}
