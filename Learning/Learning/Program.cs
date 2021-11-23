a1eaeca908ef72ec1591b6c89c14ec95520c00f3
Yong <legeous113101@gmail.com>
2021-11-23 18:20:14 +0800

    aster prosseccing

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,32 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Learning
 {
     class Program
     {
-
         public static void Main(string[] args)
         {
+            var test = new Test();
+            test.InitializeMap(10, 10);
+            
+            Console.ReadLine();
         }
     }
 
-    class Node
-    {
-        Vector2 location = new Vector2();
-        public double costFromStart;
-        public double costFromTarget;
-        public double totalCost;
-        public Node parent;
-        public bool isObstacle = false;
-        public bool isStartNode = false;
-        public bool isGoalNode = false;
-    }
-
-    class Vector2
-    {
-        public int x, y;
-    }
 }
