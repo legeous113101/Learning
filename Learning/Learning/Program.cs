2945fe582fb4421880ba2a20e73764b390afd03c
Yong <legeous113101@gmail.com>
2021-12-02 20:03:52 +0800

    hjkglahds

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -12,29 +12,8 @@ namespace Learning
     {
         public static void Main(string[] args)
         {
-            //    var test = new Test();
-            //    test.InitializeMap(10, 10);
-            //    IOTest.WriteSomething();
-
-            var s = "123 456 789";
-            var rt = s.Split(' ');
-            Console.WriteLine(rt[0]);
-            Console.WriteLine(rt[1]);
-            Console.WriteLine(rt[2]);
-            Console.ReadLine();
         }
     }
 
-    class IOTest
-    {
-        public static void WriteSomething()
-        {
-            string myPath = "C:/Users/Student/Desktop/TestStuff/abc.txt";
-            StreamWriter writer = new StreamWriter(myPath);
-            string buildString = "Hi Windows";
-            writer.WriteLine(buildString);
-            writer.Close();
-        }
-    }
 
 }
